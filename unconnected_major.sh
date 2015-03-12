756fdeecbae7aec981a040dc10cc2649a39744b5
Arun Ganesh <arun.planemad@gmail.com>
2015-03-12 23:28:00 +0530

    Update tested script

--- unconnected_major.sh
+++ unconnected_major.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
 ## Run data downloader ##
-#./download_data.sh
+./download_data.sh
 
 ## Move to data dir ##
 cd data
 
 ## Download data ##
-#echo Downloading unconnected major data
-#echo 5m errors
-#curl -S --progress-bar "http://tools.geofabrik.de/osmi/view/routing/wxs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=unconnected_major5" > unconnected_major5.gml
-#echo 2m errors
-#curl -S --progress-bar "http://tools.geofabrik.de/osmi/view/routing/wxs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=unconnected_major2" > unconnected_major2.gml
-#echo 1m errors
-#curl -S --progress-bar "http://tools.geofabrik.de/osmi/view/routing/wxs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=unconnected_major1" > unconnected_major1.gml
-
+echo Downloading unconnected major data
+echo 5m errors
+curl -S --progress-bar "http://tools.geofabrik.de/osmi/view/routing/wxs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=unconnected_major5" > unconnected_major5.gml
+echo 2m errors
+curl -S --progress-bar "http://tools.geofabrik.de/osmi/view/routing/wxs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=unconnected_major2" > unconnected_major2.gml
+echo 1m errors
+curl -S --progress-bar "http://tools.geofabrik.de/osmi/view/routing/wxs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GetFeature&TYPENAME=unconnected_major1" > unconnected_major1.gml
 
 # Merging data ##
 echo Merging data files
@@ -38,5 +37,9 @@ echo Interscting errors with country shapes
 cd ..
 python unconnected_intersect.py
 
-ogr2ogr -F "CSV" output/unconnected_major.csv data/output.shp
+## Processing output
+mkdir output
+echo Converting SHP to CSV
+rm output/unconnected_major.csv
+ogr2ogr -F "CSV" output/unconnected_major.csv data/output.shp -lco GEOMETRY=AS_XY
 
